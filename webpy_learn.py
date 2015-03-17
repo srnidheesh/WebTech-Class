13cfd8d4bc8fa6c085cd2dddf5d11cbad1361dc9
Lalitha Prasad <lalithaprasad@gmail.com>
2015-03-17 22:25:32 +0530

    Modified

--- webpy_learn.py
+++ webpy_learn.py
@@ -6,6 +6,7 @@ import sys
 urls=(
 		"/favicon.ico","Icon",
 		"/","Index",
+		"/register","Register",
 		)
 
 render=web.template.render("templates/");
@@ -31,6 +32,18 @@ class Index:
 			return render.valid(inp.username)
 		else:
 			return render.invalid(inp.username)
+
+class Register:
+	def GET(self):
+		return render.register()
+
+	def POST(self):
+		inp=web.input()
+		uid=db.insert("users",username=inp.username,password=inp.password)
+		if uid:
+			return "Registered"
+		else:
+			return "Not registered"
 
 if __name__=="__main__":
 	app=web.application(urls,globals())
